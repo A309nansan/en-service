b3b69559b47d34dc610ea1781a26f6060f631d81
octopus-kim <kk97sj@gmail.com>
2025-04-30 11:52:11 +0900

    [FEAT] enlv1s2c3jy1 로직 구현

--- src/main/java/site/nansan/en/service/problem/enlv1s2c3/enlv1s2c3jy1.java
+++ src/main/java/site/nansan/en/service/problem/enlv1s2c3/enlv1s2c3jy1.java
@@ -3,17 +3,38 @@ package site.nansan.en.service.problem.enlv1s2c3;
 import org.springframework.stereotype.Service;
 import site.nansan.en.dto.util.ProblemAnswer;
 import site.nansan.en.service.ProblemService;
+import site.nansan.global.util.CreateRandom;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class enlv1s2c3jy1 extends ProblemService {
+
     @Override
     public ProblemAnswer makeProblemAnswer() {
+
         Map<String, Object> problem = new HashMap<>();
         Map<String, Object> answer = new HashMap<>();
-        return ProblemAnswer.from(problem, answer);
+        List<Integer> startNumbers = CreateRandom.pickRandomNumbers(List.of(1, 2, 3, 4, 5), 3);
 
+        for (int p = 1; p <= 3; p++) {
+
+            int[] array = makeArray(startNumbers.get(p - 1));
+            int blankIndex = CreateRandom.generateRandomInt(1, 3);
+            String keyName = "p" + p;
+
+            answer.put(keyName, array);
+            array[blankIndex] = 0;
+            answer.put(keyName, array);
+        }
+
+        return ProblemAnswer.from(problem, answer);
+    }
+
+    private int[] makeArray(int sn) {
+
+        return new int[]{sn, sn + 1, sn + 2, sn + 3, sn + 4};
     }
 }
